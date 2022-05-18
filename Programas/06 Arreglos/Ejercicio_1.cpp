dbf10714441cd8de5ff52403cab1b591a356bcf7
ampinkus <ampinkus@gmail.com>
2022-05-18 18:28:56 -0300

    Agregado capitulo 7

--- Programas/06 Arreglos/Ejercicio_1.cpp
+++ Programas/06 Arreglos/Ejercicio_1.cpp
@@ -1,26 +1,26 @@
 // Indicar el tamaño de un arreglo por teclado y luego llenarlo de numeros aleatorios de 1 al 100
 // al final imprimir todos los valores
+// Comentario agregado para ver si aparece en Git
 
 #include <iostream>
 #include <cstdlib> 
 #include <ctime> 
-#include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	int arreglo[101]; // tamaño maximo arreglo es 100
+	int arreglo[200]; // Cambio el tamaño dle arreglo a 200 para ver como aparece en Git
 	int n; // tamaño del arreglo 
-	srand(time(0); // inicializo el generador de numeros aleatorios 
+	srand(time(NULL)); // inicializo el generador de numeros aleatorios 
 	
 	cout << "Ingrese el tamaño del arreglo n < 100: ";
 	cin >> n;
-	while (n > 0) {  
+	if (n > 0  && n < 100){  // terminar si 0 > n o n > 100  
 		for ( int i = 0 ; i < n ; i++) 
 			arreglo[i] = ( rand() % 100 ) + 1; // aleatorios entre 0 y 100 
-	}
 	
 	for ( int i = 0 ; i < n ; i++) 
-		cout << arreglo[i] << endl;
+		cout << arreglo[i] << " ";
+	}
 	
 	return 0;
 }
---
+++ Programas/07 Ordenamiento/burbuja.cpp
@@ -0,0 +1,44 @@
+// Ejercicio 1
+// Implementar un ordenamiento por metodo de burbuja
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(void)
+{
+
+    int arreglo[1000]; // arreglo para ordenar
+    int i ; // auxiliar
+    int n; // tamaño del arreglo
+    int auxiliar;
+    bool ordenado; // me indica si en el arreglo hubo o no intercambios, si no los hubo está ordenado
+    srand(time(NULL)); // inicializo el generador de numeros aleatorios
+
+    cout << "Ingresar el tamaño del arreglo a ordenar (n > 2):  ";
+    cin >> n;
+
+    for ( int i = 0 ; i < n ; i++)
+        arreglo[i] = ( rand() % 100 ) + 1; // aleatorios entre 0 y 100
+
+
+    // bubble sort
+    ordenado = false;
+    while (ordenado == false)  // Es la unica condicioon que se debe cumplir
+    {
+        ordenado = true; // presuponemos que está ordenado salvo que necesitemos hacer un intercambio de numeros
+        for (i = 0; i <= (n - 2); i++) // siempre el Hasta en este caso debe ser 2 valores menos que el numero de elementos!
+            if (arreglo[i] > arreglo[i+1])   // tengo que intercambiar los valores de i con i+1
+            {
+                auxiliar = arreglo[i];
+                arreglo[i] = arreglo[i+1];
+                arreglo[i+1] = auxiliar;
+                ordenado = false; // indico que no estaba ordenado
+            }
+    }
+
+    for ( int i = 0 ; i < n ; i++)
+        cout << arreglo[i] << " ";
+
+    return (0);
+}
