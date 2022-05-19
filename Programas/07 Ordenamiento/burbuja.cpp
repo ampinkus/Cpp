760e0570fc9ec8934d3fd0d38351463400a1b712
ampinkus <ampinkus@gmail.com>
2022-05-18 21:49:31 -0300

    Sincronizado desde linux

--- Programas/07 Ordenamiento/burbuja.cpp
+++ Programas/07 Ordenamiento/burbuja.cpp
@@ -3,19 +3,20 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
 int main(void)
 {
 
-    int arreglo[1000]; // arreglo para ordenar
+    int arreglo[2000]; // arreglo para ordenar
     int i ; // auxiliar
-    int n; // tamaÒo del arreglo
+    int n; // tama√±o del arreglo
     int auxiliar;
-    bool ordenado; // me indica si en el arreglo hubo o no intercambios, si no los hubo est· ordenado
+    bool ordenado; // me indica si en el arreglo hubo o no intercambios, si no los hubo est√° ordenado
     srand(time(NULL)); // inicializo el generador de numeros aleatorios
 
-    cout << "Ingresar el tamaÒo del arreglo a ordenar (n > 2):  ";
+    cout << "Ingresar el tama√±o del arreglo a ordenar (2 < n < 1000):  ";
     cin >> n;
 
     for ( int i = 0 ; i < n ; i++)
@@ -24,9 +25,9 @@ int main(void)
 
     // bubble sort
     ordenado = false;
-    while (ordenado == false)  // Es la unica condicioon que se debe cumplir
+    while (ordenado == false)   // Es la unica condicioon que se debe cumplir
     {
-        ordenado = true; // presuponemos que est· ordenado salvo que necesitemos hacer un intercambio de numeros
+        ordenado = true; // presuponemos que est√° ordenado salvo que necesitemos hacer un intercambio de numeros
         for (i = 0; i <= (n - 2); i++) // siempre el Hasta en este caso debe ser 2 valores menos que el numero de elementos!
             if (arreglo[i] > arreglo[i+1])   // tengo que intercambiar los valores de i con i+1
             {
